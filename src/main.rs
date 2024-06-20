b2fd522bc60bbdda004fd496c3bb840e0bec662f
Alicja Januszkiewicz <d.januszkiewicz@gmail.com>
2024-06-20 17:02:08 +0100

    avoid cloning and indirection at the cost of not owning the transition function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 #![allow(non_camel_case_types)]
 use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
-use Eq;
 use std::marker::PhantomData;
 
 pub enum Action<Γ> {
@@ -10,18 +9,22 @@ pub enum Action<Γ> {
     PopAndPush(Γ),
 }
 
-type K<Q, Σ, Γ> = (Q, Σ, Option<Γ>);
+type K<'a, Q, Σ, Γ> = (&'a Q, Σ, Option<&'a Γ>);
 type V<Q, Γ> = (Q, Action<Γ>);
-type M<Q, Σ, Γ> = HashMap<K<Q, Σ, Γ>, V<Q, Γ>>;
+type M<'a, Q, Σ, Γ> = HashMap<K<'a, Q, Σ, Γ>, V<Q, Γ>>;
 
-pub trait HashMapExt<Q, Σ, Γ> {
-    fn get(&self, k: &K<Q, Σ, Γ>) -> Option<&V<Q, Γ>>;
+pub trait HashMapExt<'a, Q, Σ, Γ> {
+    fn get(&self, k: &K<'a, Q, Σ, Γ>) -> Option<&V<Q, Γ>>;
 }
 
-impl<Q, Σ, Γ> HashMapExt<Q, Σ, Γ> for M<Q, Σ, Γ> 
-where Q: Eq + Hash, Σ: Eq + Hash, Γ: Eq + Hash, {
-    fn get(&self, k: &(Q, Σ, Option<Γ>)) -> Option<&(Q, Action<Γ>)> {
-        HashMap::get(&self, k)
+impl<'a, Q, Σ, Γ> HashMapExt<'a, Q, Σ, Γ> for M<'a, Q, Σ, Γ>
+where
+    Q: Eq + Hash,
+    Σ: Eq + Hash,
+    Γ: Eq + Hash,
+{
+    fn get(&self, k: &K<'a, Q, Σ, Γ>) -> Option<&V<Q, Γ>> {
+        HashMap::get(self, k)
     }
 }
 
@@ -29,44 +32,55 @@ where Q: Eq + Hash, Σ: Eq + Hash, Γ: Eq + Hash, {
 /// Σ is the input alphabet
 /// Γ is the stack alphabet
 /// δ is the transition function
-struct PushdownAutomaton<Q, Σ, Γ, δ = M<Q, Σ, Γ>> {
-    stack: Vec<Γ>,
-    transitions: δ,
-    start_state: Q,
-    final_states: HashSet<Q>,
-    _phantom: PhantomData<Σ>,
+struct PushdownAutomaton<'a, Q, Σ, Γ, δ = M<'a, Q, Σ, Γ>> {
+    stack: Vec<&'a Γ>,
+    start_state: &'a Q,
+    final_states: HashSet<&'a Q>,
+    transitions: &'a δ,
+    _phantom: PhantomData<Σ>
 }
 
-impl<Q, Σ, Γ, δ> PushdownAutomaton<Q, Σ, Γ, δ> 
-where δ: HashMapExt<Q, Σ, Γ>, Q: Default, Γ: Clone, Q: Clone {
-    pub fn new(start_state: Q, transitions: δ, final_states: HashSet<Q>) -> Self {
+impl<'a, Q, Σ, Γ, δ> PushdownAutomaton<'a, Q, Σ, Γ, δ>
+where
+    Q: Eq + Hash,
+    Σ: Eq + Hash,
+    Γ: Eq + Hash,
+    δ: HashMapExt<'a, Q, Σ, Γ>,
+{
+    pub fn new(
+        start_state: &'a Q,
+        final_states: HashSet<&'a Q>,
+        transitions: &'a δ,
+    ) -> Self {
         let stack = Vec::new();
-        Self {stack, transitions, start_state, final_states, _phantom: PhantomData}
+        Self {
+            stack,
+            start_state,
+            final_states,
+            transitions,
+            _phantom: PhantomData,
+        }
     }
-    pub fn transition(&mut self, input_symbol: Σ) -> core::result::Result<(), &'static str> {
-        let current_state = std::mem::take(&mut self.start_state);
+
+    pub fn transition(&mut self, input_symbol: Σ) -> Result<(), &'static str> {
         let stack_top = self.stack.pop();
-        let stack_top_clone = stack_top.clone();
-        match self.transitions.get(&(current_state, input_symbol, stack_top)) {
+
+        match self.transitions.get(&(self.start_state, input_symbol, stack_top)) {
             Some((next_state, Action::Push(symbol))) => {
-                match stack_top_clone {
-                    Some(v) => self.stack.push(v),
-                    _ => {},
+                if let Some(value) = stack_top {
+                    self.stack.push(value); // Push back the original top
                 }
-                self.stack.push(symbol.clone());
-                self.start_state = next_state.clone();
-            },
+                self.stack.push(symbol);
+                self.start_state = next_state;
+            }
             Some((next_state, Action::Pop)) => {
-                // self.stack.pop();
-                self.start_state = next_state.clone();
-            },
+                self.start_state = next_state;
+            }
             Some((next_state, Action::PopAndPush(symbol))) => {
-                // self.stack.pop();
-                // warning: will not error on an empty stack
-                self.stack.push(symbol.clone());
-                self.start_state = next_state.clone();
-            },
-            None => {return Err("Error: Transition not defined");},
+                self.stack.push(symbol);
+                self.start_state = next_state;
+            }
+            None => return Err("Error: Transition not defined"),
         };
         Ok(())
     }
@@ -76,56 +90,63 @@ fn main() {
     test_pushdown_automaton();
 }
 
-
 // Function to test the Pushdown Automaton
 fn test_pushdown_automaton() {
-    // Define states and symbols
-    #[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
-    enum State {
-        #[default]
-        Start,
-        A,
-        B,
-        C,
-    }
-
-    #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-    enum Symbol {
-        X,
-        Y,
-        Z,
-    }
-
     // Example transitions using M
-    let mut transitions: M<State, Symbol, char> = HashMap::new();
+    let mut transitions: M<State, Symbol, StackElement> = HashMap::new();
 
     // Define transitions
-    transitions.insert((State::Start, Symbol::X, None), (State::A, Action::Push('a')));
-    transitions.insert((State::A, Symbol::Y, Some('a')), (State::B, Action::Pop));
-    transitions.insert((State::B, Symbol::Z, None), (State::C, Action::PopAndPush('b')));
-    transitions.insert((State::C, Symbol::X, Some('b')), (State::Start, Action::Push('c')));
+    transitions.insert((&State::Start, Symbol::X, None), (State::A, Action::Push(StackElement::A)));
+    transitions.insert((&State::A, Symbol::Y, Some(&StackElement::A)), (State::B, Action::Pop));
+    transitions.insert((&State::B, Symbol::Z, None), (State::C, Action::PopAndPush(StackElement::B)));
+    transitions.insert((&State::C, Symbol::X, Some(&StackElement::B)), (State::Start, Action::Push(StackElement::C)));
 
     // Create PDA instance
     let final_states = HashSet::new(); // No final states defined in this example
-    let mut pda = PushdownAutomaton::new(State::Start, transitions, final_states);
+    let start_state = &State::Start;
+    let mut pda = PushdownAutomaton::new(start_state, final_states, &transitions);
 
     // Test transitions
     assert_eq!(pda.transition(Symbol::X), Ok(()));
-    assert_eq!(pda.start_state, State::A);
-    assert_eq!(pda.stack, vec!['a']);
+    assert_eq!(pda.start_state, &State::A);
+    assert_eq!(pda.stack, vec![&StackElement::A]);
 
     assert_eq!(pda.transition(Symbol::Y), Ok(()));
-    assert_eq!(pda.start_state, State::B);
-    assert_eq!(pda.stack, vec![]);
+    assert_eq!(pda.start_state, &State::B);
+    let empty_vec: Vec<&StackElement> = Vec::new();
+    assert_eq!(pda.stack, empty_vec);
 
     assert_eq!(pda.transition(Symbol::Z), Ok(()));
-    assert_eq!(pda.start_state, State::C);
-    assert_eq!(pda.stack, vec!['b']);
+    assert_eq!(pda.start_state, &State::C);
+    assert_eq!(pda.stack, vec![&StackElement::B]);
 
     assert_eq!(pda.transition(Symbol::X), Ok(()));
-    assert_eq!(pda.start_state, State::Start);
-    assert_eq!(pda.stack, vec!['b', 'c']);
+    assert_eq!(pda.start_state, &State::Start);
+    assert_eq!(pda.stack, vec![&StackElement::B, &StackElement::C]);
 
     // Test error cases
     assert_eq!(pda.transition(Symbol::Y), Err("Error: Transition not defined"));
+}
+
+// Enum definitions for the PDA
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum State {
+    Start,
+    A,
+    B,
+    C,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum Symbol {
+    X,
+    Y,
+    Z,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum StackElement {
+    A,
+    B,
+    C,
 }
