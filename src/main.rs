e0a3fe7f28adbd6363ff280bed23d430497ea370
Alicja Januszkiewicz <d.januszkiewicz@gmail.com>
2024-06-20 19:47:57 +0100

    move transitions into the struct and enable self-reference with unsafe raw pointers

--- src/main.rs
+++ src/main.rs
@@ -33,61 +33,82 @@ where
 /// Γ is the stack alphabet
 /// δ is the transition function
 struct PushdownAutomaton<'a, Q, Σ, Γ, δ = M<'a, Q, Σ, Γ>> {
-    stack: Vec<&'a Γ>,
-    start_state: &'a Q,
-    final_states: HashSet<&'a Q>,
-    transitions: &'a δ,
-    _phantom: PhantomData<Σ>
+    stack: Vec<*const Γ>,
+    start_state: *const Q,
+    final_states: HashSet<*const Q>,
+    transitions: δ,
+    _phantom: &'a PhantomData<Σ>
 }
 
 impl<'a, Q, Σ, Γ, δ> PushdownAutomaton<'a, Q, Σ, Γ, δ>
 where
-    Q: Eq + Hash,
+    Q: Eq + Hash + 'a,
     Σ: Eq + Hash,
-    Γ: Eq + Hash,
+    Γ: Eq + Hash + 'a,
     δ: HashMapExt<'a, Q, Σ, Γ>,
 {
     pub fn new(
-        start_state: &'a Q,
-        final_states: HashSet<&'a Q>,
-        transitions: &'a δ,
+        start_state: &Q,
+        final_states: HashSet<&Q>,
+        transitions: δ,
     ) -> Self {
         let stack = Vec::new();
+        let final_states: HashSet<*const Q> = unsafe { core::mem::transmute(final_states) };
         Self {
             stack,
             start_state,
             final_states,
             transitions,
-            _phantom: PhantomData,
+            _phantom: &PhantomData,
         }
     }
 
     pub fn transition(&mut self, input_symbol: Σ) -> Result<(), &'static str> {
-        let stack_top = self.stack.pop();
+        let stack_top =self.stack.pop();
+        let stack_top_ptr: Option<&Γ> = unsafe { stack_top.map(|r| &*r) };
 
-        match self.transitions.get(&(self.start_state, input_symbol, stack_top)) {
+        let start_state = unsafe { &*self.start_state as &Q };
+
+        match self.transitions.get(&(start_state, input_symbol, stack_top_ptr)) {
             Some((next_state, Action::Push(symbol))) => {
                 if let Some(value) = stack_top {
-                    self.stack.push(value); // Push back the original top
+                    self.stack.push(value as *const Γ); // Push back the original top
                 }
-                self.stack.push(symbol);
+                self.stack.push(symbol as *const Γ);
                 self.start_state = next_state;
             }
             Some((next_state, Action::Pop)) => {
                 self.start_state = next_state;
             }
             Some((next_state, Action::PopAndPush(symbol))) => {
-                self.stack.push(symbol);
+                self.stack.push(symbol as *const Γ);
                 self.start_state = next_state;
             }
             None => return Err("Error: Transition not defined"),
         };
         Ok(())
     }
+    pub fn get_state(&self) -> &Q {
+        unsafe { &*self.start_state as &Q }
+    }
+    pub fn get_stack(&self) -> Vec<&Γ> {
+        let mut stack = Vec::with_capacity(self.stack.len());
+        unsafe {        
+            stack.set_len(self.stack.len());
+    
+            core::ptr::copy_nonoverlapping(
+                self.stack.as_ptr() as *const &Γ,
+                stack.as_mut_ptr(),
+                self.stack.len(),
+            );
+    
+            stack
+        }
+    }
 }
 
 fn main() {
-    test_pushdown_automaton();
+    test_pushdown_automaton()
 }
 
 // Function to test the Pushdown Automaton
@@ -104,25 +125,25 @@ fn test_pushdown_automaton() {
     // Create PDA instance
     let final_states = HashSet::new(); // No final states defined in this example
     let start_state = &State::Start;
-    let mut pda = PushdownAutomaton::new(start_state, final_states, &transitions);
+    let mut pda = PushdownAutomaton::new(start_state, final_states, transitions);
 
     // Test transitions
     assert_eq!(pda.transition(Symbol::X), Ok(()));
-    assert_eq!(pda.start_state, &State::A);
-    assert_eq!(pda.stack, vec![&StackElement::A]);
+    assert_eq!(pda.get_state(), &State::A);
+    assert_eq!(pda.get_stack(), vec![&StackElement::A]);
 
     assert_eq!(pda.transition(Symbol::Y), Ok(()));
-    assert_eq!(pda.start_state, &State::B);
+    assert_eq!(pda.get_state(), &State::B);
     let empty_vec: Vec<&StackElement> = Vec::new();
-    assert_eq!(pda.stack, empty_vec);
+    assert_eq!(pda.get_stack(), empty_vec);
 
     assert_eq!(pda.transition(Symbol::Z), Ok(()));
-    assert_eq!(pda.start_state, &State::C);
-    assert_eq!(pda.stack, vec![&StackElement::B]);
+    assert_eq!(pda.get_state(), &State::C);
+    assert_eq!(pda.get_stack(), vec![&StackElement::B]);
 
     assert_eq!(pda.transition(Symbol::X), Ok(()));
-    assert_eq!(pda.start_state, &State::Start);
-    assert_eq!(pda.stack, vec![&StackElement::B, &StackElement::C]);
+    assert_eq!(pda.get_state(), &State::Start);
+    assert_eq!(pda.get_stack(), vec![&StackElement::B, &StackElement::C]);
 
     // Test error cases
     assert_eq!(pda.transition(Symbol::Y), Err("Error: Transition not defined"));
